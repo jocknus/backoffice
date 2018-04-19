405868150df54832c9a52ac5cfb032de46fb19b8
martinscomeon <martin.sundvall@comeon.com>
2018-04-19 09:11:52 +0200

    merge styled index-page, move script to js-folder, add preventDefault for clickhandler

--- web/js/login-box.js
+++ web/js/login-box.js
@@ -5,27 +5,28 @@
     var usernameEl = document.getElementById('username');
     var passwordEl = document.getElementById('password');
     var button = document.getElementById('submit');
-    
 
-    button.addEventListener('click', function() {
+
+    button.addEventListener('click', function(e) {
+        e.preventDefault();
         var httpRequest = new XMLHttpRequest()
         httpRequest.onreadystatechange = function (data) {
             if (httpRequest.readyState == XMLHttpRequest.DONE) {
                 console.log(httpRequest.responseText);
-                if (httpRequest.status == 200) {    
+                if (httpRequest.status == 200) {
                     window.location.href = "/app";
-                    
+
                 }else {
                     passwordEl.value = "";
                     passwordEl.placeholder = "wrong user or password";
-                    
-                }    
-            }        
+
+                }
+            }
         }
         httpRequest.open('POST', LOGIN_ENDPOINT,true);
         httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
         var requestParameters = 'flow=Login1&FORM_USERNAME=' + encodeURIComponent(usernameEl.value) + '&FORM_PASSWORD=' + encodeURIComponent(passwordEl.value);
         httpRequest.send(requestParameters);
-        
-    });       
+
+    });
 })();
