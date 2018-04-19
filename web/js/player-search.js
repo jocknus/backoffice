409a5f294f6d7155b414ef32c660bc99a0804ece
linusg <linus.gustafsson@comeon.com>
2018-04-19 21:28:44 +0200

    some ui adjustment to make it look better

--- web/js/player-search.js
+++ web/js/player-search.js
@@ -16,29 +16,35 @@
                     var data = JSON.parse(httpRequest.responseText);
                     
                     var table = document.getElementById('player-result');
-                    table.tBodies[0].innerHTML = "";    
+                    table.setAttribute("border","0");
+                    table.tBodies[0].innerHTML = "<tr><th>PID</th><th>Username</th><th>Player status id</th><th>Site_id</th><th>First name</th></tr>";    
                    
                     data.forEach(function (s){
                         // Id
                         var row = document.createElement("tr");
                         
                         var tdId = document.createElement("td");
+                        tdId.setAttribute('align', 'center');
                         tdId.appendChild(document.createTextNode(s.id));
                         row.appendChild(tdId);
                         
                         var tdUsername = document.createElement("td");
+                        tdUsername.setAttribute('align', 'center');
                         tdUsername.appendChild(document.createTextNode(s.username));
                         row.appendChild(tdUsername);
                         
                         var tdSite = document.createElement("td");
+                        tdSite.setAttribute('align', 'center');
                         tdSite.appendChild(document.createTextNode(s.site));
                         row.appendChild(tdSite);
 
                         var tdEmail = document.createElement("td");
+                        tdEmail.setAttribute('align', 'center');
                         tdEmail.appendChild(document.createTextNode(s.email));
                         row.appendChild(tdEmail);
 
                         var tdLastlogin = document.createElement("td");
+                        tdLastlogin.setAttribute('align', 'center');
                         tdLastlogin.appendChild(document.createTextNode(s.lastlogin));
                         row.appendChild(tdLastlogin);
 
